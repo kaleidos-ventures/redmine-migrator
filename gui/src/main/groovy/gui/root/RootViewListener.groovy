35a16034e79b061814a12a9e443bf0b1f48f8d8f
Mario Garcia <mario.ggar@gmail.com>
2014-10-15 11:21:43 +0200

    Show Settings first time app starts #1304

--- gui/src/main/groovy/gui/root/RootViewListener.groovy
+++ gui/src/main/groovy/gui/root/RootViewListener.groovy
@@ -4,6 +4,10 @@ import static org.viewaframework.util.ComponentFinder.find
 import static org.viewaframework.util.ResourceLocator.getImageIcon
 
 import javax.swing.JTextField
+
+import gui.settings.SettingsView
+import gui.settings.SettingsService
+
 import org.viewaframework.view.event.ViewContainerEvent
 import org.viewaframework.view.event.DefaultViewContainerEventController
 
@@ -11,8 +15,23 @@ class RootViewListener extends DefaultViewContainerEventController {
 
     @Override
     public void onViewInitUIState(ViewContainerEvent event) {
+        changeGlassPaneToTranslucent(event)
+    }
+
+    @Override
+    public void onViewFinalUIState(ViewContainerEvent event) {
+        showSettingsIfNeccesary(event)
+    }
+
+    void changeGlassPaneToTranslucent(ViewContainerEvent event) {
         def translucentPane = new TranslucentGlassPane(event.source)
         event.source.rootPane.glassPane = translucentPane
+    }
+
+    void showSettingsIfNeccesary(ViewContainerEvent event) {
+        if (!SettingsService.configFile.exists()) {
+            event.source.application.viewManager.addView(new SettingsView())
+        }
     }
 
 }
